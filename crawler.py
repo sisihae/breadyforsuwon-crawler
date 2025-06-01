929af58de77623ce70c2d9b6beb04365e368ab66
sisihae <sihyunkim@snu.ac.kr>
2025-06-01 16:40:38 +0900

    rename: change var names

--- crawler.py
+++ crawler.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 url = "https://map.kakao.com/"
 driver.get(url)
 
-searchloc = '수원 빵집집'
+searchloc = '수원 빵집'
 
 search_area = driver.find_element(By.XPATH, r'//*[@id="search.keyword.query"]') # 카카오맵 검색창
 search_area.send_keys(searchloc)  # 검색어 전달
@@ -22,19 +22,19 @@ driver.find_element(By.XPATH, r'//*[@id="info.main.options"]/li[2]/a').send_keys
 
 bakery_list = [] 
 
-def roomNamePrint():
+def bakeryNamePrint():
     time.sleep(0.2)
     
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     
-    room_lists = soup.select('.placelist > .PlaceItem') # 장소 정보를 모두 가져옴
-    for i, room in enumerate(room_lists):
+    bakery_lists = soup.select('.placelist > .PlaceItem') # 장소 정보를 모두 가져옴
+    for i, bakery in enumerate(bakery_lists):
         temp = []
         
-        name = room.select('.head_item > .tit_name > .link_name')[0].text
-        score = room.select('.rating > .score > em')[0].text
-        addr = room.select('.addr > p')[0].text
+        name = bakery.select('.head_item > .tit_name > .link_name')[0].text
+        rating = bakery.select('.rating > .score > em')[0].text
+        addr = bakery.select('.addr > p')[0].text
         
         # 상세정보 탭으로 이동
         driver.find_element(By.XPATH, r'//*[@id="info.search.place.list"]/li['+str(i+1)+']/div[5]/div[4]/a[1]').send_keys(Keys.ENTER)
@@ -42,14 +42,13 @@ def roomNamePrint():
         time.sleep(2)
         rev = extract_review()  # 리뷰 추출 함수 실행
         
-        
-        # 하나의 리스트로 만들어 room_list에 추가
+        # 하나의 리스트로 만들어 bakery_list에 추가
         temp.append(name)
-        temp.append(score)
+        temp.append(rating)
         temp.append(addr[3:])
         temp.append(rev)
-        
-        room_list.append(temp)
+        bakery_list.append(temp)
+
 
 def extract_review():
     html = driver.page_source
@@ -69,7 +68,6 @@ def extract_review():
     # 없으면 빈칸 추가
     else:
         rev.append(' ')
-        
     
     # 다시 검색 탭으로 전환
     driver.close()
@@ -86,7 +84,6 @@ for i in range(1, 9):
     try:
         page2 += 1
         print(page, 'page')
-        
         # 페이지 버튼 번호(1에서 5 사이 값)
         if i > 5:
             xpath = '/html/body/div[5]/div[2]/div[1]/div[7]/div[6]/div/a['+str(i-5)+']'
@@ -94,8 +91,7 @@ for i in range(1, 9):
             xpath = '/html/body/div[5]/div[2]/div[1]/div[7]/div[6]/div/a['+str(i)+']'
         
         driver.find_element(By.XPATH, xpath).send_keys(Keys.ENTER)  # 페이지 선택
-        roomNamePrint()  # 장소 정보 크롤링
-        
+        bakeryNamePrint()  # 장소 정보 크롤링     
         
         # page2가 5를 넘어가면 다시 1로 바꿔주고 다음 버튼 클릭
         if page2 > 5:
@@ -107,4 +103,4 @@ for i in range(1, 9):
     except:
         break
         
-print('크롤링 완료')
+print('crawling done')
