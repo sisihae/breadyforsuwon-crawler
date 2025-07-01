4c23c51a2ebe74382e1dc8cfbcb68336cfde6592
sisihae <sihyunkim@snu.ac.kr>
2025-07-01 22:11:49 +0900

    feat: crawler edit

--- crawler.py
+++ crawler.py
@@ -6,7 +6,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import csv
 
+driver = webdriver.Chrome()
 url = "https://map.kakao.com/"
 driver.get(url)
 
@@ -16,71 +18,81 @@ search_area = driver.find_element(By.XPATH, r'//*[@id="search.keyword.query"]') 
 search_area.send_keys(searchloc)  # 검색어 전달
 driver.find_element(By.XPATH, r'//*[@id="search.keyword.submit"]').send_keys(Keys.ENTER)  # 돋보기 클릭
 
-time.sleep(5)
+time.sleep(2)
 
 driver.find_element(By.XPATH, r'//*[@id="info.main.options"]/li[2]/a').send_keys(Keys.ENTER)  # 장소 탭
+
+time.sleep(5)
 
 bakery_list = [] 
 
 def bakeryNamePrint():
-    time.sleep(0.2)
-    
+    time.sleep(2)
+
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    
-    bakery_lists = soup.select('.placelist > .PlaceItem') # 장소 정보를 모두 가져옴
-    for i, bakery in enumerate(bakery_lists):
+
+    bakery_items = soup.select('.placelist > .PlaceItem')
+
+    for i, bakery in enumerate(bakery_items):
         temp = []
-        
-        name = bakery.select('.head_item > .tit_name > .link_name')[0].text
-        rating = bakery.select('.rating > .score > em')[0].text
-        addr = bakery.select('.addr > p')[0].text
-        
-        # 상세정보 탭으로 이동
-        driver.find_element(By.XPATH, r'//*[@id="info.search.place.list"]/li['+str(i+1)+']/div[5]/div[4]/a[1]').send_keys(Keys.ENTER)
+
+        name = bakery.select_one('.head_item > .tit_name > .link_name').text.strip()
+        rating = bakery.select_one('.rating > .score > em').text.strip()
+        addr = bakery.select_one('.addr > p').text.strip()
+
+        # 상세페이지 링크 추출
+        try:
+            detail_link_tag = bakery.select_one('.moreview')
+            detail_url = detail_link_tag['href']  # e.g., https://place.map.kakao.com/1924550713
+            review_url = detail_url + "#comment"
+        except:
+            print(f"[경고] {name} 상세페이지 링크 추출 실패")
+            continue
+
+        # 새 창에서 리뷰 페이지 열기
+        driver.execute_script(f"window.open('{review_url}');")
         driver.switch_to.window(driver.window_handles[-1])
         time.sleep(2)
-        rev = extract_review()  # 리뷰 추출 함수 실행
-        
-        # 하나의 리스트로 만들어 bakery_list에 추가
+
+        rev = extract_review()
+
         temp.append(name)
         temp.append(rating)
         temp.append(addr[3:])
         temp.append(rev)
         bakery_list.append(temp)
 
+        # 리뷰 창 닫고 돌아가기
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(1)
+
 
 def extract_review():
+    time.sleep(2)
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    
-    # 후기 목록 찾기
-    review_lists = soup.select('.list_evaluation > li')
-    
-    count = 0
+
+    review_lists = soup.select('.list_review > li') 
     rev = []
-    # 리뷰가 있는 경우
-    if len(review_lists) != 0:
+
+    if review_lists:
         for review in review_lists:
-            comment = review.select('.txt_comment > span')[0].text  # 리뷰
-            if len(comment) != 0:
-                rev.append(comment)
-    # 없으면 빈칸 추가
+            comment_tag = review.select_one('.desc_review > p')
+            if comment_tag:
+                rev.append(comment_tag.text.strip())
     else:
         rev.append(' ')
-    
-    # 다시 검색 탭으로 전환
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-    time.sleep(2)
-    
+
     return rev
+
 
 
 page = 1  # 현재 페이지
 page2 = 1  # 5개 중 몇번째인지(버튼이 5개씩있어서 6번째가 되면 다시 1로 바꿔줘야함)
 
-for i in range(1, 9):
+for i in range(1, 2):
     try:
         page2 += 1
         print(page, 'page')
@@ -100,7 +112,14 @@ for i in range(1, 9):
         
         page += 1
     
-    except:
+    except Exception as e:
+        print(f"페이지 {i} 에서 에러 발생: {e}")
         break
         
 print('crawling done')
+
+with open('bakery_list.csv', 'w', newline='', encoding='utf-8-sig') as f:
+    writer = csv.writer(f)
+    writer.writerow(['이름', '평점', '주소', '리뷰'])  
+    for row in bakery_list:
+        writer.writerow([row[0], row[1], row[2], ' | '.join(row[3])])
